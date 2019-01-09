f06046e89b51ed846b8fa9c2b4abd46c8a62afde
Wing Huang <huangsen365@gmail.com>
2019-01-09 17:16:47 +0800

    modified:   test_boto3.py

--- test_boto3.py
+++ test_boto3.py
@@ -1,6 +1,14 @@
-import boto3
+import boto3, json, csv
 
 ec2 = boto3.client('ec2')
 response = ec2.describe_instances()
+# print response
+# print type(response)
+with open('response.csv', 'wb') as f:
+    w = csv.DictWriter(f, response.keys())
+    w.writeheader()
+    w.writerow(response)
+# response_parsed = json.loads(response)
+#print(json.dump(response, indent=4, sort_keys=True))
 # print(type(response))
 # print(response)
