33be33fa85f86f166a026c97e6567ac0f60b75c5
Wing Huang <huangsen365@gmail.com>
2019-01-09 17:24:52 +0800

    modified:   docker_bash_snippets.sh

--- docker_bash_snippets.sh
+++ docker_bash_snippets.sh
@@ -1,2 +1,3 @@
-#docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 ash
-docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
+# docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 ash
+# docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
+docker run --rm -it -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
