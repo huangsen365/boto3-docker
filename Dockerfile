ce3bdb5db1d5d7465afede9acc2034acdf851d77
Wing Huang <huangsen365@gmail.com>
2019-01-09 18:44:55 +0800

    modified:   Dockerfile 	modified:   docker_bash_snippets.sh

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM python:2.7-alpine
 MAINTAINER huangsen365@gmail.com
 
-RUN pip install boto3 awscli
+RUN apk add man man-pages
 RUN apk add vim
+RUN pip install boto3 awscli
+#RUN echo -e "export HISTSIZE=20000\nexport HISTTIMEFORMAT=\"%F %T \"" >> /etc/bachrc
 COPY ll_color_auto.sh /etc/profile.d/
+#COPY long_history.sh /etc/profile.d/
 WORKDIR /root/dev
 
 CMD ["python"]
--- docker_bash_snippets.sh
+++ docker_bash_snippets.sh
@@ -1,3 +1,4 @@
 # docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 ash
 # docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
-docker run --rm -it -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
+# docker run --rm -it -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
+docker run --rm -it -v /root/Z:/root/Z -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
