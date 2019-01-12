067bd72624043f2cbb4d81c96db4893cec70daf6
Wing Huang <huangsen365@gmail.com>
2019-01-12 23:31:56 +0800

    modified:   Dockerfile 	modified:   docker_bash_snippets.sh

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ MAINTAINER huangsen365@gmail.com
 
 RUN apk add man man-pages
 RUN apk add vim
+RUN apk add tzdata
+RUN rm -rf /etc/localtime
+RUN ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 RUN pip install boto3 awscli
+#RUN pip install aws-shell
 #RUN echo -e "export HISTSIZE=20000\nexport HISTTIMEFORMAT=\"%F %T \"" >> /etc/bachrc
 COPY ll_color_auto.sh /etc/profile.d/
 #COPY long_history.sh /etc/profile.d/
--- docker_bash_snippets.sh
+++ docker_bash_snippets.sh
@@ -1,4 +1,6 @@
 # docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 ash
 # docker run --rm -it -v /root/Z/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Z/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
 # docker run --rm -it -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
-docker run --rm -it -v /root/Z:/root/Z -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
+#docker run --rm -it -v /root/Z:/root/Z -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
+docker run --rm -it -v /root/Z/Projects/CloudCMDB-AWS/.ash_history:/root/.ash_history -v /root/Z:/root/Z -v /root/Projects/github.com/huangsen365/boto3-docker:/root/dev/boto3-docker -v /root/Projects/github.com/huangsen365/boto3-docker/data/.aws:/root/.aws --name boto3_20190109 myboto3 sh -l
+
